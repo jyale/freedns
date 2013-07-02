e7a16e776a0ed8898bdc7c3509cc60c30f0af1e7
John <john@smorz.cs.yale.edu.fake>
2013-07-02 17:16:01 -0400

    log ip addresses of socks clients

--- AuthSOCKS.py
+++ AuthSOCKS.py
@@ -5,12 +5,13 @@ from twisted.python import log
 class AuthSOCKS(socks.SOCKSv4Factory):
 	def __init__(self, logfile):
 		# print logfile
+		log.startLogging(open('weak.log', 'w'))
 		socks.SOCKSv4Factory.__init__(self, logfile)   
 	   
 	def buildProtocol(self, addr):
 		# the ip address of the user connecting to the socks proxy
 		ipaddr = addr.host
-		# print ipaddr
+		print ipaddr
 		# limit socks proxy access to yale network
 		# yale ip address range from:
 		# http://its.yale.edu/how-to/finding-your-ip-and-network-hardware-addresses
@@ -18,6 +19,8 @@ class AuthSOCKS(socks.SOCKSv4Factory):
 		# uncomment following line to limit access to Yale network
 
 		# if ipaddr.startswith('130.132.') or ipaddr.startswith('128.36.'):
+		# log all connections
+		log.msg("New client connected: " + ipaddr)
 		return socks.SOCKSv4Factory.buildProtocol(self,addr)
 				
 	   
