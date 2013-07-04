5334e91ed79d6e3d75fad9d845ad6ba3037f8118
John <john@smorz.cs.yale.edu.fake>
2013-07-04 02:23:40 -0400

    don't delete ip log

--- runscript.sh
+++ runscript.sh
@@ -5,6 +5,9 @@
 # kill the processes if they're already running
 pkill python
 pkill twistd
+rm twistd.log
+rm socks.log
+# rm weak.log
 # now run the new DNS server
 twistd -y secondtestserver.py
 # run the Socks proxy
