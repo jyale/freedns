ac8bc9f76678c2b2b955ee2c7151d8fc80c8840d
John <john@smorz.cs.yale.edu.fake>
2013-07-05 21:45:18 +0000

    send messages to phones tests

--- sms/code.php
+++ sms/code.php
@@ -1,10 +1,28 @@
 <?php
+
 $number = $_POST["number"];
 $ip = $_POST["ipaddr"];
-file_put_contents($number,$ip);
-echo exec("./write.py " + $number + " " + $ip);
+
+$code = uniqid();
+
+file_put_contents($number,$code);
+
+// Download/Install the PHP helper library from twilio.com/docs/libraries.
+// This line loads the library
+require('twilio/Services/Twilio.php');
+// Your Account Sid and Auth Token from twilio.com/user/account
+$sid = file_get_contents("sid");
+$token = file_get_contents("token");
+$client = new Services_Twilio($sid, $token);
+
+echo $sid;
+	$message = $client->account->sms_messages->create("+12037120067", $number, $code, array());
+	echo $message->sid;
+	echo '<br>weak<br>';
 
 ?>
+
+
 
 <html>
 <body>
