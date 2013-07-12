9d26b9a6f2d282f63379c88c35a3eed1c2f6974a
John <john@smorz.cs.yale.edu.fake>
2013-07-12 01:36:28 +0000

    did frontend of first page

--- sms/registerphone.php
+++ sms/registerphone.php
@@ -1,13 +1,40 @@
+<!DOCTYPE html>   
+<html lang="en">   
+<head>   
+<meta charset="utf-8">   
+<title>Register cellphone</title>   
+<meta name="description" content="weak">   
+<link href="assets/css/bootstrap.css" rel="stylesheet">  
 
-<html>
-<body>
+</head>  
+<body>  
 
-Enter your cellphone number and IP address:
-<form action="code.php" method="post">
-Cellphone number: <input type="text" name="number"><br>
-IP address: <input type="text" name="ipaddr">
-<input type="submit">
-</form>
 
-</body>
-</html> 
+
+<form class='form-horizontal' enctype="multipart/form-data" action="code.php" method="POST">
+<fieldset>
+ <legend>Enter your cellphone number and IP address: </legend>
+
+  <div class="control-group">  
+		
+	     <label class="control-label" for="area">Cellphone number: </label>  
+            <div class="controls">  
+              <input class="text" name="number"></input>  
+            </div>  
+          </div>  
+
+  <div class="control-group">  
+		
+	     <label class="control-label" for="area">IP address: </label>  
+            <div class="controls">  
+              <input class="text" name="ipaddr"></input>  
+            </div>  
+<div class="form-actions">  
+<button type="submit" class="btn btn-primary">Next &raquo</button>  
+</div>         
+        </fieldset> 
+
+</form>  
+</body>  
+</html>  
+
