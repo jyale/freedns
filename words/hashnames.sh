5f6f59f5f514446949f52284cabb65e46880b0da
John <john@smorz.cs.yale.edu.fake>
2013-07-21 10:27:34 -0400

    script to hash domain names

---
+++ words/hashnames.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# script to read a file and lookup all .com domains and dump name and IP
+# to output file
+
+# cat weak | xargs -0 -I % echo %
+
+# dig google.com +short | head -n 1
+
+while read p; do
+  a='.com';
+  q=$p$a;
+  out=$(dig $q +short | head -n 1);
+  done=$q", "$out;
+  echo $done >> weak.out;
+  echo $done;
+done < domains.csv
